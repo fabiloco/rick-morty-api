1f527459f968c69e7d51ab95d7367d6c8c23f460
Fabián Sánchez <faalsaru@gmail.com>
2025-04-02 12:32:11 -0500

    feat(characterByFilter): character query by filters

--- src/schema/querys.ts
+++ src/schema/querys.ts
@@ -1,4 +1,4 @@
-import { GraphQLObjectType, GraphQLList, GraphQLInt } from "graphql";
+import { GraphQLObjectType, GraphQLList, GraphQLInt, GraphQLString } from "graphql";
 import { Character } from "../models/character";
 import { CharacterType } from "./types/characterType";
 
@@ -18,6 +18,28 @@ export const query = new GraphQLObjectType({
       },
       resolve: async (_, { id }) => {
         return await Character.findByPk(id, { raw: true });
+      },
+    },
+    charactersByFilter: {
+      type: new GraphQLList(CharacterType),
+      args: {
+        name: { type: GraphQLString },
+        status: { type: GraphQLString },
+        species: { type: GraphQLString },
+        gender: { type: GraphQLString },
+        origin: { type: GraphQLString },
+      },
+      resolve: async (_, args) => {
+        const whereClause: any = {};
+
+        if (args.name) whereClause.name = args.name;
+        if (args.status) whereClause.status = args.status;
+        if (args.species) whereClause.species = args.species;
+        if (args.gender) whereClause.gender = args.gender;
+        if (args.origin) whereClause.origin = args.origin;
+
+        const res = await Character.findAll({ where: whereClause });
+        return res.map((character) => character.get({ plain: true }));
       },
     },
   },
